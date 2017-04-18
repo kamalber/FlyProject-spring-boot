355fc562c8a1083fd0f2a11676e87ab08ddf6946
kamal <kamal.berriga@gmail.com>
2017-04-18 10:52:09 +0100

    add class util/poalrity for sentiment analysis

---
+++ src/main/java/com/weberfly/util/Polarity.java
@@ -0,0 +1,27 @@
+package com.weberfly.util;
+
+public class Polarity {
+
+	private int positive;
+	private int negative;
+	private int  neutral;
+	public int getPositive() {
+		return positive;
+	}
+	public void setPositive(int positive) {
+		this.positive = positive;
+	}
+	public int getNegative() {
+		return negative;
+	}
+	public void setNegative(int negative) {
+		this.negative = negative;
+	}
+	public int getNeutral() {
+		return neutral;
+	}
+	public void setNeutral(int neutral) {
+		this.neutral = neutral;
+	}
+	
+}
