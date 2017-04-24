4a795e14df6d4ddbc40ec0b0afdbe0dfc5547a54
Hicham EL Mimouni <hichamelmimouni.3@gmail.com>
2017-04-24 16:49:31 +0100

    commit test apps

--- src/main/java/com/weberfly/service/PostService.java
+++ src/main/java/com/weberfly/service/PostService.java
@@ -1,10 +1,12 @@
 package com.weberfly.service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.weberfly.dao.CategoryItemRepository;
 import com.weberfly.dao.PostRepository;
 import com.weberfly.entities.Category;
 import com.weberfly.entities.CategoryItem;
@@ -14,11 +16,20 @@ import com.weberfly.entities.Post;
 public class PostService {
 	@Autowired
 	private PostRepository postRepository;
+	@Autowired
+	private CategoryItemRepository categoryItemRepository;
 	
 	public void savePost(Post post) {
-//		for (CategoryItem item : ) {
-//			
-//		}
+		String pos =post.getContent();
+		List<CategoryItem> categoryItems =categoryItemRepository.findAll();
+		List<CategoryItem> pubcategoryItems =new ArrayList<>();
+		for (CategoryItem item : categoryItems) {		
+			if (pos.toLowerCase().contains(item.getName().toLowerCase())){		
+				pubcategoryItems.add(item);	
+		}
+		}
+		post.setCategoryItems(pubcategoryItems);
+		
 		postRepository.save(post);
 	}
 	public List<Post> getAll() {
