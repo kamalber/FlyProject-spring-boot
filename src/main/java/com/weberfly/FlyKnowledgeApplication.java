d5fbb28388e6f1f59463f932f1e3d14d3ef48a46
Hicham EL Mimouni <hichamelmimouni.3@gmail.com>
2017-05-11 19:40:49 +0100

    commit resolve error

--- src/main/java/com/weberfly/FlyKnowledgeApplication.java
+++ src/main/java/com/weberfly/FlyKnowledgeApplication.java
@@ -7,8 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
@@ -22,20 +22,20 @@ public class FlyKnowledgeApplication {
 		SpringApplication.run(FlyKnowledgeApplication.class, args);
 	}
 	
-	@Configuration
-	@Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
-	protected static class SecurityConfiguration extends WebSecurityConfigurerAdapter {
-		@Override
-		protected void configure(HttpSecurity http) throws Exception {
-			// @formatter:off
-			http
-				.httpBasic().and()
-				.authorizeRequests()
-					.antMatchers("/index.html", "/home.html", "/login.html", "/").permitAll()
-					.anyRequest().authenticated();
-					
-			// @formatter:on
-		}
-	}
+//	@Configuration
+//	@Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
+//	protected static class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+//		@Override
+//		protected void configure(HttpSecurity http) throws Exception {
+//			// @formatter:off
+//			http
+//				.httpBasic().and()
+//				.authorizeRequests()
+//					.antMatchers("/index.html", "/home.html", "/login.html", "/").permitAll()
+//					.anyRequest().authenticated();
+//					
+//			// @formatter:on
+//		}
+//	}
 	
 }
