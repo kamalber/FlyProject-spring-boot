5a12fd746e110198eba19100390e8db0a940704e
kamal <kamal.berriga@gmail.com>
2017-05-21 12:06:50 +0000

    spring security configuration webConfig/userDetails

--- src/main/java/com/weberfly/dao/UserRepository.java
+++ src/main/java/com/weberfly/dao/UserRepository.java
@@ -6,6 +6,8 @@ import org.springframework.stereotype.Repository;
 import com.weberfly.entities.User;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+
+public User findOneByUsername(String username);
 	 
 
  
