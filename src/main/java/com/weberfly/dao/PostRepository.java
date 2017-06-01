ff36c6d6fe761a46354dbf8d733722f4cad02123
kamal <kamal.berriga@gmail.com>
2017-06-01 23:25:31 +0000

    changing stats params search

--- src/main/java/com/weberfly/dao/PostRepository.java
+++ src/main/java/com/weberfly/dao/PostRepository.java
@@ -7,13 +7,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import org.springframework.stereotype.Repository;
 
-import com.weberfly.entities.CategoryItem;
+
 import com.weberfly.entities.Post;
 
 @Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
 
 	
-	public List<Post> findByCategoryItemsAndDateBetween(List<CategoryItem> items,Date startDate,Date endDate);
+	public List<Post> findByTitleAndDateBetween(String title,Date startDate,Date endDate);
 	
 }
