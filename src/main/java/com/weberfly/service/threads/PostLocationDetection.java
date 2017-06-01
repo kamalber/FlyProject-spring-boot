e8abfd5aa35b818df5b4df4fd77f5db943cbc10b
kamal <kamal.berriga@gmail.com>
2017-06-01 15:40:25 +0000

    Post location ip detection config

--- src/main/java/com/weberfly/service/threads/PostLocationDetection.java
+++ src/main/java/com/weberfly/service/threads/PostLocationDetection.java
@@ -2,16 +2,22 @@ package com.weberfly.service.threads;
 
 import java.io.InputStream;
 import java.net.URL;
+import java.security.Principal;
+
 import org.json.simple.JSONObject;
 import org.json.simple.JSONValue;
 import org.apache.commons.io.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 
 import com.weberfly.entities.Country;
 import com.weberfly.entities.Location;
+import com.weberfly.entities.Session;
+import com.weberfly.entities.User;
 import com.weberfly.service.CountryService;
 import com.weberfly.service.LocationService;
-
+import com.weberfly.service.SessionService;
+import com.weberfly.service.UserService;
 
 public class PostLocationDetection extends Thread {
 	public static final String USER_AGENT = "Mozilla/5.0";
@@ -19,24 +25,19 @@ public class PostLocationDetection extends Thread {
 	LocationService locationService;
 	@Autowired
 	CountryService countryService;
+	@Autowired
+	UserService userService;
+	User connectd;
 
-	private volatile boolean running = true;
-	private String ipAdresse;
-
-	public PostLocationDetection(String ipAdresse) {
-		this.ipAdresse = ipAdresse;
-	}
-
-	public void arreter() {
-		this.running = false;
+	public PostLocationDetection() {
+		this.connectd= (User)SecurityContextHolder.getContext().getAuthentication().getPrincipal();
 	}
 
 	@Override
 	public void run() {
-
 		// traitement du thread
 		try {
-			getLocationByIpAdresse(this.ipAdresse);
+			getLocationByIpAdresse();
 			System.out.println("thread finish");
 		} catch (Exception ex) {
 			ex.printStackTrace();
@@ -44,41 +45,54 @@ public class PostLocationDetection extends Thread {
 
 	}
 
-	public Location getLocationByIpAdresse(String ip) throws Exception {
+	public Location getLocationByIpAdresse() throws Exception {
 		Location location = null;
-		System.out.println(ip);
-		 location=locationService.find(ip);
+		Country country =null;
 
-		if (location != null) {// the location already exist with the same or					
-			return location;  // other user session
-		} else {// create the location , probably this is the first time
-				// connection by the current user
-			String url = "http://ip-api.com/json/" + ip;
-			try {
-				location = new Location();
-				location.setIp(ip);
-				InputStream stream = new URL(url).openStream();
-				String genreJson = IOUtils.toString(stream);
-				JSONObject genreJsonObject = (JSONObject) JSONValue.parseWithException(genreJson);
+		String url = "http://ip-api.com/json/";
+		try {
 
-				double latitude = (double) genreJsonObject.get("lat");
-				double longitude = (double) genreJsonObject.get("lon");
-				System.out.println((String) genreJsonObject.get("countryCode"));
-				Country country = countryService.find((String) genreJsonObject.get("countryCode"));
+			InputStream stream = new URL(url).openStream();
+			String genreJson = IOUtils.toString(stream);
+			JSONObject genreJsonObject = (JSONObject) JSONValue.parseWithException(genreJson);
 
-				location.setCountry(country);
-				location.setLatitude(latitude);
-				location.setLongitude(longitude);
-				System.out.print(location);
-				locationService.save(location);
-
-			} catch (Exception e) {
-				e.printStackTrace();
+			String ipAdress = (String) genreJsonObject.get("query");
+			Session session=new Session();
+			session.setIpUser(ipAdress);
+		
+            this.connectd.setSession(session);
+            userService.update(this.connectd);
+            
+			location = locationService.find(ipAdress);
+			if (location != null) {// the location already exist with the same
+									// or
+				return location;
 			}
+			location = new Location();
+			location.setIp(ipAdress);
+			double latitude = (double) genreJsonObject.get("lat");
+			double longitude = (double) genreJsonObject.get("lon");
+			System.out.println((String) genreJsonObject.get("countryCode"));
+			 country = countryService.find((String) genreJsonObject.get("countryCode"));
+
+			location.setCountry(country);
+			location.setLatitude(latitude);
+			location.setLongitude(longitude);
+			System.out.println(locationService.findLast());
+			locationService.save(location);
 			
-			System.out.println(location.getLongitude());
-			return location;
+			
+		} catch (Exception e) {
+			// if the ip is not valide we set default location to the 
+			//created post by searchyin from th user session with ip 127.0.0.1;
+			System.out.println("error while loeading location");
+			Session session=new Session();
+			this.connectd.setSession(session);
+			userService.update(this.connectd);
+			
 		}
 
+		return location;
 	}
+
 }
